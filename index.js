e4ae7b978efceb095137c05fdb40d8949fe166be
Pedro Gonzalez <orion@Pedros-MacBook-Pro.local>
2024-03-16 15:21:14 -0500

    finished the questions method and the questions it will prompt user.

--- index.js
+++ index.js
@@ -11,3 +11,26 @@ const questions = [
         message: 'What shape did you want the svg to be?',
         choices: ['circle', 'triangle', 'square']
     },
+    //prompts me for the shape color
+    //enters a color keyboard
+    {
+        type:'input',
+        message: 'What color did you want the shape to be?',
+        name: 'shape_color'
+    },
+    //prompts for a text
+    //enter three characters
+    {
+        type: 'input',
+        message: 'Enter character set of three letters:',
+        name: 'text'
+    },
+    //prompts for the text color
+    //eter a color keyboard
+    {
+        type: 'input',
+        message: 'What color would you like the text to be?',
+        name: 'text_color'
+    },
+];
+

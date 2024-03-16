1aa51f3f2c5f988b74fc2f66341a95e0babf49cf
Pedro Gonzalez <orion@Pedros-MacBook-Pro.local>
2024-03-16 15:19:51 -0500

    modified the index.js file to include the questions method that will prompt the command line to ask questions.

--- index.js
+++ index.js
@@ -2,3 +2,12 @@ const inquirer = require('inquirer');
 const fs = require('fs');
 const generateLogo = require('./utils/generateLogo');
 
+const questions = [ 
+    //when I am prompted for a shape
+    //then I am presented with a list of shapes to choose from: circle, triangle, and square.
+    {
+        type: 'list',
+        name: 'shape',
+        message: 'What shape did you want the svg to be?',
+        choices: ['circle', 'triangle', 'square']
+    },

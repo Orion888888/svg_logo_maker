911fa0882e869408cb2c17648bc2a9a9cc6ae8dd
Pedro Gonzalez <orion@Pedros-MacBook-Pro.local>
2024-03-16 15:55:56 -0500

    added comments to the generateLogo.js file to explain the processing of the code.

--- utils/generateLogo.js
+++ utils/generateLogo.js
@@ -1,7 +1,23 @@
+//imports the different classes from files located in the 'lib' folder.
 const Square = require("../lib/squre")
 const Rectangle = require("../lib/rectangle")
 const Circle = require("../lib/circle")
 
+//defines a function named  generateLogo  that generates a logo based on the input data
+//function decides which shape to use (Triangle, Square, or Circle) for the logo based on the  shape  property of the input data.
 function generateLogo(data) {
-    
+    //initializes new variable
+    let shape = undefined
+    // The function checks the  shape  property of the input  data  object to determine which shape to use for the logo. 
+    if (data.shape === 'Triangle') {
+        shape = new Triangle(data.shape_color, data.text, data.text_color)
+    } else if (data.shape === 'Square') {
+        shape = new Square(data.shape_color, data.text, data.text_color)
+    } else {
+        shape = new Circle(data.shape_color, data.text, data.text_color)
+    }
+    //calls the  render  method on the created shape object to generate the logo and returns the result.
+    return shape.render();
 }
+//Exports the  generateLogo  function so it can be used in other parts of the application. 
+module.exports = generateLogo;
